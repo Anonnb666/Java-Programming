18f9ec53d7ec58c41534a91cbd3d0ab437db2e6a
Anonnb666 <1760107928@qq.com>
2024-09-20 16:01:23 +0800

    Add files via upload

---
+++ Chapter1/e109.java
@@ -0,0 +1,7 @@
+public class e109 {
+    public static void main(String[] args) {
+        double width=4.5,height=7.9,area;
+        area=width*height;
+        System.out.println("area:"+area);
+    }
+}
