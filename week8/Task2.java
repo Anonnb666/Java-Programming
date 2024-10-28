5d741803f5127930c6414ce3654d2c5337ae4c64
Anonnb666 <1760107928@qq.com>
2024-10-28 10:50:01 +0800

    Add files via upload

---
+++ week8/Task2.java
@@ -0,0 +1,13 @@
+package week8;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] mylist=new int[10];
+        for (int i = 0; i < mylist.length; i++) {
+            mylist[i]=sc.nextInt();
+        }
+    }
+}
